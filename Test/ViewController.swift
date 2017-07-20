60655707f7137e985ab11c2c871792c6e2899c17
Benedikt Wilhelm <benedikt.wilhelm@gmx.de>
2017-07-20 15:18:03 +0200

    now we can add numbers

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -12,15 +12,18 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var label1: UILabel!
     
-    var tapCount = 0
+    @IBOutlet weak var text1: UITextField!
+    
+    @IBOutlet weak var text2: UITextField!
+    
+    
     
     @IBAction func buttonTapped(_ sender: Any) {
         
-        tapCount += 1
+      print(text1.text!)
+      print(text2.text!)
         
-        if tapCount >= 20 {
-            label1.text = "You tapped the button 20 times"
-        }
+        label1.text = "The answer is... \(Double(text1.text!)! + Double(text2.text!)!)"
         
     }
     
